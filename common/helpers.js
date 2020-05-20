29bc849bf34dd6e7637823c26a15280ce929991a
Raluca Filimon <filimon.raluca@gmail.com>
2020-05-20 17:34:36 +0300

    changed origin

--- common/helpers.js
+++ common/helpers.js
@@ -37,7 +37,10 @@ module.exports.success = (res, data = null, statusCode = OK) => {
 
 const addHeaders = (res) => {
   res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, OPTIONS, PATCH, DELETE"
+  );
   return res;
 };
