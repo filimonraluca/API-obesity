2a6398442fe54ccac9701faece58581da81c7621
Raluca Filimon <filimon.raluca@gmail.com>
2020-05-20 16:53:49 +0300

    mesaj

--- config/index.js
+++ config/index.js
@@ -1,17 +1,17 @@
 const PORT = 3001 || parseInt(process.env.PORT, 10);
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 // Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const envFound = dotenv.config();
 if (!envFound) {
-	// This error should crash whole process
+  // This error should crash whole process
 
-	throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 module.exports = {
-  PORT: ,
+  PORT: PORT,
   MONGO_CONNECTION_STRING:
     "mongodb+srv://andra-raluca:Bisl2k5pczRc4kPK@ip-project-yau3a.mongodb.net/obesity_usa?authSource=admin&replicaSet=ip-project-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
 };
